c8632366b0db0f50e7ef2b997845e1b6cb1eb701
Jakob <jakob.have@gmail.com>
2017-01-21 10:26:46 +0100

    Axis input from controller. Character forward an rotation
    WIP stuff

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float moveSpeed = 5;
+    public float rotationSpeed = 10;
+    [HideInInspector]
+    public float turnAxisInput;
+
+    private CharacterController CC;
+	// Use this for initialization
+	void Awake ()
+    {
+        CC = transform.GetComponent<CharacterController>(); 
+
+    }
+
+    private void Start()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        MoveCharacter();
+    }
+
+    void MoveCharacter()
+    {
+        Vector3 moveVector = transform.forward * moveSpeed * Time.deltaTime;
+        transform.Rotate( Vector3.up * turnAxisInput * rotationSpeed * Time.deltaTime);
+
+
+        CC.Move( moveVector );
+    }
+    
+}
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+    public Character character;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        character.turnAxisInput = Input.GetAxis( "Horizontal" );
+    }
+}
